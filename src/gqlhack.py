67e15d544bdfd7053554473cb975d9b9ec65cbe3
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2020-04-29 18:13:41 +0900

    :sparkles: Add pipenv files.

---
+++ src/gqlhack.py
@@ -0,0 +1,21 @@
+def welcome():
+  print("""          
+          |   |              |    
+,---.,---.|   |---.,---.,---.|__/ 
+|   ||   ||   |   |,---||    |  \ 
+`---|`---|`-  `   '`---^`---'`   `
+`---'    |   
+
+GraphQL pentest tool.
+
+Author: Tsubasa Umeuchi (Szarny)
+Repository: https://github.com/Szarny/gqlhack
+""")
+
+
+def main():
+  welcome()
+
+
+if __name__ == '__main__':
+  main()
